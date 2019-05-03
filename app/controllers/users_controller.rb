15c0eb4942d4ab727eaa486300bfe183fbc20618
Laura Felce <crepu83@yahoo.it>
2019-05-03 10:58:42 +0000

    login and logout routes and rendering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,35 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect to '/'
+    end
+  end
 
+  post 'login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/'
+      end
+   end
 
+   get '/logout' do
+     if logged_in?
+       session.clear
+      redirect to "/login"
+      else
+        redirect to "/"
+      end
+    end
 
+    get '/users/:slug'
+    @user = @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
 
 
 end
