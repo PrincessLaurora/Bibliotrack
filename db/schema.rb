257256a4ba9f51f2bb9eeb7f124a4865886d8ac8
Laura Felce <crepu83@yahoo.it>
2019-05-06 17:43:00 +0000

    added user id column in books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_02_144150) do
+ActiveRecord::Schema.define(version: 2019_05_06_174022) do
 
   create_table "books", force: :cascade do |t|
     t.string "author"
     t.text "title"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
