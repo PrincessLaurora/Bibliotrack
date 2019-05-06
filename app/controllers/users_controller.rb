76417959f02f7c7b0f993974c5fe6a90be110e56
Laura Felce <crepu83@yahoo.it>
2019-05-06 10:15:52 +0000

    sorted books by alphabetical order in users/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
 
     get '/users/:slug' do
       if logged_in?
-      @books = Book.all
+      @books = Book.order(:author)
       @user = User.find_by_slug(params[:slug])
       erb :'users/show'
       end
