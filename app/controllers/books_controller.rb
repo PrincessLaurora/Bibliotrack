27902800afc511dac44ba472a80e3849c6c3234c
Laura Felce <crepu83@yahoo.it>
2019-05-06 09:33:02 +0000

    improved users-show with links and defined edit route

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -34,7 +34,34 @@ class BooksController < ApplicationController
    end
 
     get '/books/:id/edit' do
-
+      if logged_in?
+        if @book = current_user.books.find_by(id: params[:id])
+          erb :'books/edit'
+        else
+          redirect to "/"
+        end
+      else
+        redirect to "/login"
+      end
     end
+
+    patch '/books/:id' do
+      if logged_in?
+        @book = Book.find_by(id: params[:id])
+        if params[:author] == "" || params[:title] == ""
+          redirect to ('/books/#{@book.id}/edit')
+        else
+          if @book = current_user.books.find_by(id: params[:id])
+            @book.update(:author => params[:author], :title => params[:title])
+              redirect to ('/books/#{@book.id}')
+          else
+            redirect to "/"
+          end
+        end
+      else
+        redirect to "/login"
+      end
+    end
+ 
 
 end
